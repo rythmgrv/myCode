30fa16b846f671664214a524e736398cf90b6525
rythmgrv <rythm.grv1@gmail.com>
2017-01-13 16:54:17 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'abc-123',
+        server: 'localhost', 
+        database: 'Education750' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT TOP 1 * FROM [Education750].[dbo].[somast]', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
